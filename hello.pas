75c4cdcfb7389faa6115a12447c56da3a4d3341e
Анопка Артем <veselchak24@bk.ru>
2023-09-30 09:04:13 +0300

    hello.pas - task 1

--- hello.pas
+++ hello.pas
@@ -1,3 +1,13 @@
 ﻿begin
-  Print('Hello world!');
+  var time := ReadInteger('Введите текущий час:');
+  
+  Assert(time in 0..24);
+  
+  case time of
+    4..10: Print('Доброе утро, мир!');
+    11..16: Print('Добрый день, мир!');
+    17..22: Print('Добрый вечер, мир!')
+  else
+       Print('Доброй ночи, мир!');
+  end;
 end.
