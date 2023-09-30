3faedcb7208c581c02af8ddc15a311131d612425
Анопка Артем <veselchak24@bk.ru>
2023-09-30 09:15:08 +0300

    task 3

--- time.pas
+++ time.pas
@@ -2,7 +2,7 @@
   var year := abs(ReadInteger('Введите текущий год:'));
   
   if (year mod 4 =0) and not((year mod 100=0) and (year mod 400 <> 0))then
-    Print('Год високосный')
+    Print('В этом году 366 дней')
   else
-    Print('Год НЕ високосный');    
+    Print('В этом году 365 дней');
 end.
---
+++ timer.pas
@@ -0,0 +1,7 @@
+﻿begin
+  var hours := ReadInteger('Введите количество часов:');
+  var minutes := ReadInteger('Введите количество минут:');
+  var seconds := ReadInteger('Введите количество секунд:');
+
+ Print('Суммарное количество секунд:',hours*3600+minutes*60+seconds);
+end.
