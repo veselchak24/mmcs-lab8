c13bb4847207f59c5c4de48c1e2a62fc36f1345d
Анопка Артем <veselchak24@bk.ru>
2023-09-30 09:09:25 +0300

    task 2

---
+++ time.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var year := abs(ReadInteger('Введите текущий год:'));
+  
+  if (year mod 4 =0) and not((year mod 100=0) and (year mod 400 <> 0))then
+    Print('Год високосный')
+  else
+    Print('Год НЕ високосный');    
+end.
